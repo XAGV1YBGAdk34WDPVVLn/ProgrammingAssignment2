e92303998c6783b25ae6acd7810a1ca0badb554d
XAGV1YBGAdk34WDPVVLn <XAGV1YBGAdk34WDPVVLn@users.noreply.github.com>
2017-03-01 18:30:43 +0400

    Update cachematrix.R
    added comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,12 +1,13 @@
-## Put comments here that give an overall description of what your
-## functions do
+# The following functions attempt to improve efficiency by not re-computing the inverse
+# of a matrix, and instead returning a cached version a previously solved inversed matrix.
 
-## Write a short comment describing this function
-
+# function contains accessor like functions that cache a matrix and cache the inverse of the inverse matrix
+# returns a list of the accessor functions
 makeCacheMatrix <- function(x = matrix()) {
     m <- NULL
 
     set <- function(y){
+        # assign in the parent environment
         x <<- y
         m <<- NULL
     }
@@ -23,6 +24,7 @@ makeCacheMatrix <- function(x = matrix()) {
         m
     }
 
+    # return the accessor functions
     list(
         set = set,
         get = get,
@@ -32,21 +34,21 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
-
+# computes and returns the inverse of a matrix created by the makeCacheMatrix.
+# if the matrix inverse has already been computed, then a cached version is returned to avoid the computation cost
 cacheSolve <- function(x, ...) {
-    ## Return a matrix that is the inverse of 'x'
+    # check to see if we already computed a matrix from makeCacheMatrix
     m <- x$getinverse()
 
     if(!is.null(m)){
          message("Getting cached data")
+        # if exists, no need to solve the inverse, return the cached value
         return(m)
     }
 
+    # otherwise we need to compute the inverse using solve
     data <- x$get()
-
     m <- solve(data, ...)
-
     x$setinverse(m)
 
     return(m)
